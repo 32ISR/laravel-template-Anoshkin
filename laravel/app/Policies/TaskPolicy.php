c308e17deadaecda0b088438674c705547db243c
Hyvka <anfibia.zloy@mail.ru>
2026-05-21 08:04:16 +0000

    ololol

---
+++ laravel/app/Policies/TaskPolicy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+
+class TaskPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function viewAny(User $user)
+    {
+        return false;
+    }
+
+    public function view(User $user)
+    {
+        return false;
+    }
+
+    public function update(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function resotre()
+    {
+        return false;
+    }
+
+    public function forceDelete()
+    {
+        return false;
+    }
+
+    public function __construct()
+    {
+        //
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +20,7 @@ Route::post('/login', [LoginController::class, 'store']);
 
 
 Route::post('/logout', LogoutController::class)->name('logout');
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('tasks', TaskController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
