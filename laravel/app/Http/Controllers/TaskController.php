df715c898da80e1df064af83220f21d11c30b94e
Hyvka <anfibia.zloy@mail.ru>
2026-05-25 11:19:39 +0000

    olaamigos

--- laravel/app/Http/Controllers/TaskController.php
+++ laravel/app/Http/Controllers/TaskController.php
@@ -3,20 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
     public function index()
     {
+        /** @var User $user */
+        $user = Auth::user();
+        $tasks = $user
+            ->tasks()
+            ->with('category')
+            ->latest()
+            ->paginate(10);
 
+            return view('tasks.index', compact('tasks'));
     }
-    public function store()
+    public function store(Request $request)
     {
-
+        $data = $request->validate([
+            "title"     => "string|max:255",
+            "description"    => "string|nullable",
+            "status" => "required|in:pending,in_progress,done",
+            "priority" => "required|in:low,medium,high",
+            "due_date" => "nullable|after_or_equal:today",
+            "category_id" => "nullable|exists:categories:id"
+        ]);
+        /** @var User $user */
+        $user = Auth::user();
+        $user->tasks()->create($data);
+        return redirect()->route('tasks.index')->with('success', 'Задача создана');
     }
     public function create()
     {
-
+        /** @var User $user */
+        $user = Auth::user();
+        $categories = $user->categories()->get();
+        return view('tasks.create', compact('categories'));
     }
     public function edit()
     {
--- laravel/app/Models/Task.php
+++ laravel/app/Models/Task.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+        'category_id',
+        'user_id'
+    ];
+    public function tasks()
+    {
+        return $this->hasMany
+        (Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo
+        (User::class);
+    }
+    
 }
